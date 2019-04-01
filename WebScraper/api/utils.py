7c6e18bfa606ca0d6684584a39f15972a0af21d4
Milosz Sobiczewski <sobiczewsk.milosz@gmail.com>
2019-04-01 19:08:08 +0200

    comments added

--- WebScraper/api/utils.py
+++ WebScraper/api/utils.py
@@ -6,6 +6,11 @@ import requests
 
 
 def get_conn(url):
+    """
+    Try to connect to url, return True if success
+    :param url:
+    :return:
+    """
     try:
         request = requests.get(url)
         if request.status_code == 200:
@@ -18,6 +23,11 @@ def get_conn(url):
 
 
 def make_soup(url):
+    """
+    get BeautifulSoup object
+    :param url:
+    :return:
+    """
     if get_conn(url):
         # get url response object
         http = PoolManager()
@@ -32,7 +42,7 @@ def get_txt(url):
     """
     Get text from given url.
     :param url:
-    :return:
+    :return: scraped text
     """
     soup = make_soup(url)
 
@@ -50,6 +60,12 @@ def get_txt(url):
 
 
 def save_txt(file_name, url):
+    """
+    Save scraped text to .txt file
+    :param file_name:
+    :param url:
+    :return:
+    """
     text = get_txt(url)
     try:
         with open(file_name, "w") as text_file:
@@ -61,6 +77,11 @@ def save_txt(file_name, url):
 
 
 def get_images(url):
+    """
+    Look for images for given url
+    :param url:
+    :return: found images links
+    """
     soup = make_soup(url)
     # this makes a list of bs4 element tags
     images = [img for img in soup.findAll('img')]
@@ -78,6 +99,12 @@ def get_images(url):
 
 
 def save_img(url, subdir=''):
+    """
+    save found images
+    :param url:
+    :param subdir:
+    :return: operation status, list of saved images
+    """
     links = get_images(url)
     if len(links) > 0:
         images = [STATIC_URL[1:] + 'webscraper/' + subdir +
